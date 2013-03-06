8f36022c6dc00f8d8cbaa0d4ff12b4171411023b
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-03-05 16:35:10 -0800

    adding YARD documentation for PollexObject, PollexClass, and Scraper

--- lib/pollex/pollex_class.rb
+++ lib/pollex/pollex_class.rb
@@ -1,7 +1,9 @@
 module Pollex
-  # helper instance methods
+  # Superclass for Pollex objects providing helper instance methods.
   class PollexObject
-    # taken from https://github.com/neweryankee/nextbus/blob/master/lib/instantiate_with_attrs.rb
+    # Initializes objects with a hash of attributes.
+    # @see https://github.com/neweryankee/nextbus/blob/master/lib/instantiate_with_attrs.rb
+    # @author neweryankee
     def initialize(attrs={})
       super()
       attrs.each do |name, value|
@@ -11,6 +13,9 @@ module Pollex
       self
     end
 
+    # Overrides <tt>Object#inspect</tt> to only show the attributes defined
+    # by <tt>PollexClass#attr_inspector</tt>.
+    # @see PollexClass#attr_inspector
     def inspect
       inspectables = self.class.inspectables
       if inspectables
@@ -21,10 +26,13 @@ module Pollex
     end
   end
 
-  # helper class methods
+  # Provides helper class methods for Pollex classes.
   module PollexClass
     attr_reader :inspectables
 
+    # Defines the list of attributes whose values are displayed by <tt>PollexObject#inspect</tt>.
+    # @param *attrs [Array<Symbol>] array of attribute labels
+    # @see PollexObject#inspect
     def attr_inspector(*attrs)
       @inspectables = attrs
     end
--- lib/pollex/scraper.rb
+++ lib/pollex/scraper.rb
@@ -2,28 +2,53 @@ require 'singleton'
 require 'open-uri'
 
 module Pollex
+  # Singleton object for scraping Pollex, caching the results, and extracting data.
   class Scraper
     include Singleton
 
+    attr_accessor :verbose
+
+    # Instantiates a cache of size 100 for storing scraped pages.
     def initialize()
       @cache = LRUCache.new(:max_size => 100, :default => nil)
+      @verbose = false
     end
 
-    def open_from_cache(path)
+    # Opens the given Pollex page, either by retrieving it from the cache
+    # or by making a request with Nokogiri and then storing it in the cache.
+    # @param path [String] relative path from <tt>http://pollex.org.nz</tt>
+    # @return [Nokogiri::HTML::Document] the requested page, parsed with Nokogiri
+    def open_with_cache(path)
       if @cache[path]
-        puts "Opening cached contents of http://pollex.org.nz#{path} ..."
+        if @verbose
+          puts "Opening cached contents of http://pollex.org.nz#{path} ..."
+        end
         @cache[path]
       else
-        puts "Connecting to http://pollex.org.nz#{path} ..."
+        if @verbose
+          puts "Connecting to http://pollex.org.nz#{path} ..."
+        end
         page = Nokogiri::HTML(open("http://pollex.org.nz#{path}"))
         @cache[path] = page
         page
       end
     end
 
-    # gets arbitrary data from page by xpath, with optional post-processing
+    # Gets arbitrary data from a page, with optional post-processing.
+    # @param path [String] relative path from <tt>http://pollex.org.nz</tt>
+    # @param attr_infos [Array<Array<Symbol, String, (Proc, nil)>>] an array that,
+    #   for each element to be scraped, contains an array of:
+    #   * a key for the element
+    #   * the XPath to the element, from the <tt>div#content</tt> tag of the page
+    #   * (optionally) a Proc to be performed on the element's contents
+    # @return [Array<Symbol, String>] array of key-value pairs
+    # @example Return information about the level of a given reconstruction
+    #   Scraper.instance.get(@reconstruction_path, [
+    #     [:level_token, "table[1]/tr[2]/td/a/text()", lambda {|x| x.split(':')[0]}],
+    #     [:level_path, "table[1]/tr[2]/td/a/@href"]
+    #   ])
     def get(path, attr_infos)
-      page = open_from_cache(path)
+      page = open_with_cache(path)
       contents = page.css('#content')
 
       attrs = {}
@@ -39,10 +64,32 @@ module Pollex
       attrs
     end
 
-    # gets all elements from table by xpath, with optional post-processing
+    # Gets all elements from a table within a page, with optional post-processing.
+    # The results are returned as either an array of key-value pairs or as an
+    # array of objects, if a klass is specifed. If more than one page of results is
+    # found, the first page of results is returned as a PaginatedArray.
+    # @param klass [Class] (optional) class of objects to be instantiated
+    # @param path [String] relative path from <tt>http://pollex.org.nz</tt>
+    # @param attr_infos [Array<Array<Symbol, String, (Proc, nil)>>] an array that,
+    #   for each element to be scraped, contains an array of:
+    #   * a key for the element
+    #   * the XPath to the element, from a given table
+    #   * (optionally) a Proc to be performed on the element's contents
+    # @param table_num [Integer] the number of the table on the page to process
+    #   (default: 0 - that is, the first table on the page)
+    # @return [Array<klass>] if one page of results was found
+    # @return [PaginatedArray<klass>] if multiple pages of results were found
+    # @return [Array<Array<Symbol, String>>] if no klass is specified
+    # @example Return an array of all SemanticFields in Pollex
+    #   Scraper.instance.get_all(SemanticField, "/category/", [
+    #     [:id, 'td[1]/a/text()'],
+    #     [:path, 'td[1]/a/@href'],
+    #     [:name, 'td[2]/a/text()'],
+    #     [:count, 'td[3]/text()']
+    #   ])
     def get_all(klass, path, attr_infos, table_num = 0)
       puts "Connecting to http://pollex.org.nz#{path} ..."
-      page = Nokogiri::HTML(open("http://pollex.org.nz#{path}"))
+      page = open_with_cache(path)
 
       rows = page.css('table')[table_num].css('tr')
       objs = rows[1..-1].map do |row|
@@ -81,7 +128,7 @@ module Pollex
     end
   end
 
-  # array with a pointer to the next page of results
+  # Array with an optional pointer to the next page of results
   class PaginatedArray < Array
     attr_accessor :next_page, :query
 
@@ -93,6 +140,9 @@ module Pollex
       str
     end
 
+    # Returns the next page of results, if one exists
+    # @return PaginatedArray<@query[:klass]>
+    # @see Scraper#get_all
     def more
       if @next_page
         Scraper.instance.get_all(query[:klass], @next_page, query[:attr_infos], query[:table_num])
