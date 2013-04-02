e20ce46010e4b818b744f9fe6e38b566087a9bd5
Alex Nisnevich <alex.nisnevich@gmail.com>
2013-04-02 05:58:26 -0700

    version 0.2.0 - SemanticField changes

--- lib/pollex/semantic_field.rb
+++ lib/pollex/semantic_field.rb
@@ -3,8 +3,13 @@ module Pollex
   class SemanticField < PollexObject
     extend PollexClass
 
-    attr_accessor :id, :name, :path, :count
-    attr_inspector :id, :name, :count, :path
+    attr_accessor :code, :name, :path, :count
+    attr_inspector :id, :code, :name, :count, :path
+
+    # @return [Integer] Pollex's internal ID for this SemanticField
+    def id
+      @path.split('/').last.to_i
+    end
 
     # Returns all Reconstructions corresponding to this SemanticField
     # @return [Array<Reconstruction>] array of Reconstructions corresponding to this SemanticField
@@ -21,7 +26,7 @@ module Pollex
     # @return [Array<SemanticField>] array of SemanticFields in Pollex
     def self.all
       @semantic_fields ||= Scraper.instance.get_all(SemanticField, "/category/", [
-        [:id, 'td[1]/a/text()'],
+        [:code, 'td[1]/a/text()'],
         [:path, 'td[1]/a/@href'],
         [:name, 'td[2]/a/text()'],
         [:count, 'td[3]/text()']
@@ -34,12 +39,19 @@ module Pollex
       self.all.count
     end
 
+    # Looks up SemanticField corresponding to a given internal ID
+    # @param id [Integer] ID of SemanticField to find
+    # @return [SemanticField]
+    def self.find(id)
+      self.all.select { |sf| sf.id == id }
+    end
+
     # Looks up all SemanticFields matching a given name.
     # @note Pollex has no built-in search for SemanticFields, so this method is
     #   simply a filter over SemanticField.all.
     # @param name [String] term to search for
     # @return [Array<SemanticField>] array of SemanticFields matching the search term
-    def self.find(name)
+    def self.find_by_name(name)
       self.all.select { |sf| sf.name.downcase.include?(name.downcase) }
     end
   end
